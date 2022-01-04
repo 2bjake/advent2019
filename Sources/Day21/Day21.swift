b8634d8cab24a205269676788814e5fe49967896
Jake <2bjake@users.noreply.github.com>
2022-01-04 08:52:18 -0600

    day 21 part 1 solved

--- Sources/Day21/Day21.swift
+++ Sources/Day21/Day21.swift
@@ -1,9 +1,41 @@
-import Algorithms
+import IntCode
 
-public func partOne() {
 
+
+func run(_ script: String) {
+  var machine = Machine(program: input)
+  let result = machine.run(initialInputs: script.map { Int($0.asciiValue!) })
+
+  print(String(result.output.compactMap(UnicodeScalar.init).map(Character.init)))
+  print(result.output.last!)
 }
 
-public func partTwo() {
+let firstScript = """
+NOT B J
+NOT C T
+OR J T
+AND D T
+NOT A J
+OR T J
+WALK
 
+"""
+
+public func partOne() {
+  run(firstScript)
+}
+
+let secondScript = """
+NOT B J
+NOT C T
+OR J T
+AND D T
+NOT A J
+OR T J
+RUN
+
+"""
+
+public func partTwo() {
+  run(secondScript)
 }
